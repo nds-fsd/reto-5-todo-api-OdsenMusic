78983d6f0c7d9d91ee2618292a03694e557a47a9
Joel Oliver Millán <jolivermillan@gmail.com>
2023-11-14 13:36:47 +0100

    finished backend

---
+++ src/controller/todo.js
@@ -0,0 +1,71 @@
+const { tasks } = require("../data");
+
+function findTask(req) {
+  let id = req.params.id;
+  id = parseFloat(id);
+  let task = tasks.find((task) => task.id === id);
+  return task;
+}
+
+function findTaskIndex(req) {
+  let id = req.params.id;
+  id = parseFloat(id);
+  let taskIndex = tasks.findIndex((task) => task.id === id);
+  return taskIndex;
+}
+
+function getAllTasks(req, res) {
+  res.status(200).json(tasks);
+}
+
+function createTask(req, res) {
+  const body = req.body;
+  const newTask = {
+    id: tasks.length + 1,
+    text: body.text,
+    date: new Date(),
+    done: false,
+    group: "none",
+    color: "white",
+  };
+  tasks.push(newTask);
+  res.status(201).json(newTask);
+}
+
+function getTask(req, res) {
+  let task = findTask(req);
+
+  if (task) {
+    res.status(200).json(task);
+  } else {
+    res.status(404).send();
+  }
+}
+
+function updateTask(req, res) {
+  let task = findTask(req);
+  if (task) {
+    task.text = req.body.text;
+    res.status(200).json(task);
+  } else {
+    res.status(404).send();
+  }
+}
+
+function deleteTask(req, res) {
+  let taskIndex = findTaskIndex(req);
+  if (taskIndex === -1) {
+    res.status(404).send();
+  } else {
+    tasks.splice(taskIndex, 1);
+    res.status(204).send();
+  }
+}
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+};
--- src/data/index.js
+++ src/data/index.js
@@ -1,16 +1,18 @@
-exports.todos = [{
-  id: 0,
-  text: "Hacer la cama",
-  fecha: new Date("2022-05-21"),
-  done: false
-},
-{ 
-  id: 1,
-  text: "Ir al super",
-  fecha: new Date("2022-05-14"),
-  done: true
-}];
-
-
-
-
+exports.tasks = [
+  {
+    id: 1,
+    text: "Hacer la cama",
+    date: new Date("2022-05-21"),
+    done: false,
+    group: "none",
+    color: "white",
+  },
+  {
+    id: 2,
+    text: "Ir al super",
+    date: new Date("2022-05-14"),
+    done: true,
+    group: "none",
+    color: "white",
+  },
+];
