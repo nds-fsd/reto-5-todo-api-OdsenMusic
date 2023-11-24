8552b57e2fc190a2db2208a86d50ebf77d940a24
Joel Oliver Millán <jolivermillan@gmail.com>
2023-11-24 17:52:08 +0100

    modified data

--- src/data/groups.js
+++ src/data/groups.js
@@ -2,12 +2,12 @@ exports.groups = [
   {
     id: 1,
     name: "Programación",
-    color: "yellow",
+    color: "blue",
   },
   {
     id: 2,
     name: "Vida",
-    color: "blue",
+    color: "yellow",
   },
   {
     id: 3,
